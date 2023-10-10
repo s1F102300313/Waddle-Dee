f5eae23023cb59f3352ef2f8fcb2e8b9ee1b2504
root <root@LAPTOP-AL8R1EFG>
2023-10-10 18:35:33 +0900

    星の見た目を改善した

--- src/component/Parasol/Star/Star/Star.tsx
+++ src/component/Parasol/Star/Star/Star.tsx
@@ -2,11 +2,11 @@ import type { ParasolProps } from '../../../../types/type';
 import { StarShadow } from '../StarShadow/StarShadow';
 import styles from './Star.module.css';
 
-export const Star = ({ isParasolMove }: ParasolProps) => (
+export const Star = ({ isParasolMove, isJump }: ParasolProps) => (
   <div className={styles.starBase}>
     <div className={isParasolMove ? styles.starBorderAnimation : styles.starBorder}>
       <div className={isParasolMove ? styles.starAnimation : styles.star}>
-        <StarShadow isParasolMove={isParasolMove} />
+        <StarShadow isParasolMove={isParasolMove} isJump={isJump} />
       </div>
     </div>
   </div>
