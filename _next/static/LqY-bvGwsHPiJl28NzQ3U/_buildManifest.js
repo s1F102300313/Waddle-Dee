855ec097df5e95e0d54d8066490f592accc88197
s1F102300313 <s1F102300313@users.noreply.github.com>
2023-10-15 12:42:48 +0000

    deploy: 3743bde3d7b221d6d6a61b01f7c5757402a41ca8

--- _next/static/LqY-bvGwsHPiJl28NzQ3U/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/e88a22ea466036b8.css","static/chunks/pages/index-ef07d6fcdaa7abf6.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
